2a00a1a89be4db463aae1c78f20c1c81b8acb6c3
Devlin Duldulao <webmasterdevlin@gmail.com>
2026-05-10 15:43:23 +0200

    updated

--- src/api/client/client/types.gen.ts
+++ src/api/client/client/types.gen.ts
@@ -7,14 +7,17 @@ import type {
   AxiosResponse,
   AxiosStatic,
   CreateAxiosDefaults,
-} from "axios";
+} from 'axios';
 
-import type { Auth } from "../core/auth.gen";
-import type { ServerSentEventsOptions, ServerSentEventsResult } from "../core/serverSentEvents.gen";
-import type { Client as CoreClient, Config as CoreConfig } from "../core/types.gen";
+import type { Auth } from '../core/auth.gen';
+import type {
+  ServerSentEventsOptions,
+  ServerSentEventsResult,
+} from '../core/serverSentEvents.gen';
+import type { Client as CoreClient, Config as CoreConfig } from '../core/types.gen';
 
 export interface Config<T extends ClientOptions = ClientOptions>
-  extends Omit<CreateAxiosDefaults, "auth" | "baseURL" | "headers" | "method">, CoreConfig {
+  extends Omit<CreateAxiosDefaults, 'auth' | 'baseURL' | 'headers' | 'method'>, CoreConfig {
   /**
    * Axios implementation. You can use this option to provide either an
    * `AxiosStatic` or an `AxiosInstance`.
@@ -25,7 +28,7 @@ export interface Config<T extends ClientOptions = ClientOptions>
   /**
    * Base URL for all requests made by this client.
    */
-  baseURL?: T["baseURL"];
+  baseURL?: T['baseURL'];
   /**
    * An object containing any HTTP headers that you want to pre-populate your
    * `Headers` object with.
@@ -34,23 +37,35 @@ export interface Config<T extends ClientOptions = ClientOptions>
    */
   headers?:
     | AxiosRequestHeaders
-    | Record<string, string | number | boolean | (string | number | boolean)[] | null | undefined | unknown>;
+    | Record<
+        string,
+        string | number | boolean | (string | number | boolean)[] | null | undefined | unknown
+      >;
   /**
    * Throw an error instead of returning it in the response?
    *
    * @default false
    */
-  throwOnError?: T["throwOnError"];
+  throwOnError?: T['throwOnError'];
 }
 
-export interface RequestOptions<TData = unknown, ThrowOnError extends boolean = boolean, Url extends string = string>
+export interface RequestOptions<
+  TData = unknown,
+  ThrowOnError extends boolean = boolean,
+  Url extends string = string,
+>
   extends
     Config<{
       throwOnError: ThrowOnError;
     }>,
     Pick<
       ServerSentEventsOptions<TData>,
-      "onRequest" | "onSseError" | "onSseEvent" | "sseDefaultRetryDelay" | "sseMaxRetryAttempts" | "sseMaxRetryDelay"
+      | 'onRequest'
+      | 'onSseError'
+      | 'onSseEvent'
+      | 'sseDefaultRetryDelay'
+      | 'sseMaxRetryAttempts'
+      | 'sseMaxRetryDelay'
     > {
   /**
    * Any body that you want to add to your request.
@@ -89,16 +104,16 @@ export type RequestResult<
     >;
 
 type MethodFn = <TData = unknown, TError = unknown, ThrowOnError extends boolean = false>(
-  options: Omit<RequestOptions<TData, ThrowOnError>, "method">,
+  options: Omit<RequestOptions<TData, ThrowOnError>, 'method'>,
 ) => RequestResult<TData, TError, ThrowOnError>;
 
 type SseFn = <TData = unknown, TError = unknown, ThrowOnError extends boolean = false>(
-  options: Omit<RequestOptions<never, ThrowOnError>, "method">,
+  options: Omit<RequestOptions<never, ThrowOnError>, 'method'>,
 ) => Promise<ServerSentEventsResult<TData, TError>>;
 
 type RequestFn = <TData = unknown, TError = unknown, ThrowOnError extends boolean = false>(
-  options: Omit<RequestOptions<TData, ThrowOnError>, "method"> &
-    Pick<Required<RequestOptions<TData, ThrowOnError>>, "method">,
+  options: Omit<RequestOptions<TData, ThrowOnError>, 'method'> &
+    Pick<Required<RequestOptions<TData, ThrowOnError>>, 'method'>,
 ) => RequestResult<TData, TError, ThrowOnError>;
 
 type BuildUrlFn = <
@@ -108,7 +123,11 @@ type BuildUrlFn = <
     url: string;
   },
 >(
-  options: TData & Pick<RequestOptions<unknown, boolean>, "axios" | "baseURL" | "paramsSerializer" | "querySerializer">,
+  options: TData &
+    Pick<
+      RequestOptions<unknown, boolean>,
+      'axios' | 'baseURL' | 'paramsSerializer' | 'querySerializer'
+    >,
 ) => string;
 
 export type Client = CoreClient<RequestFn, Config, MethodFn, BuildUrlFn, SseFn> & {
@@ -141,5 +160,5 @@ export type Options<
   TData extends TDataShape = TDataShape,
   ThrowOnError extends boolean = boolean,
   TResponse = unknown,
-> = OmitKeys<RequestOptions<TResponse, ThrowOnError>, "body" | "path" | "query" | "url"> &
-  ([TData] extends [never] ? unknown : Omit<TData, "url">);
+> = OmitKeys<RequestOptions<TResponse, ThrowOnError>, 'body' | 'path' | 'query' | 'url'> &
+  ([TData] extends [never] ? unknown : Omit<TData, 'url'>);
