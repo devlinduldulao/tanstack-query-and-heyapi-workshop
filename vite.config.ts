664acbc64616d9aa7a931d2341037c1f13954974
Devlin Duldulao <webmasterdevlin@gmail.com>
2026-05-14 00:24:47 +0200

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
